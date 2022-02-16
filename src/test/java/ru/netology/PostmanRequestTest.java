6da0f2122ad840fd26b3ef469976206febc83a7b
Vasilii Lysenkov <vlysenkov@griddynamics.com>
2022-02-16 22:06:02 +0300

    body () check

---
+++ src/test/java/ru/netology/PostmanRequestTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanRequestTest {
+    @Test
+    void shouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+//                .assertThat()
+                .body(  "", Matchers.equalTo("") );
+
+    }
+}
